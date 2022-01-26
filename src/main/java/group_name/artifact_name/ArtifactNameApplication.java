21dffb9cedebe83974ab03cfbe84d2e6211abd3d
BlossomWhale <lkorea12345@gmail.com>
2022-01-26 23:33:24 +0900

    o.o

---
+++ src/main/java/group_name/artifact_name/ArtifactNameApplication.java
@@ -0,0 +1,13 @@
+package group_name.artifact_name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group_name/artifact_name/ArtifactNameApplicationTests.java
@@ -0,0 +1,13 @@
+package group_name.artifact_name;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
