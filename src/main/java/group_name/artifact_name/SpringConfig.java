f956f0a466eb21e5b5ff1e51d035e1feb0cc83ed
BlossomWhale <lkorea12345@gmail.com>
2022-01-29 15:40:17 +0900

    o.o until jpa

--- src/main/java/group_name/artifact_name/SpringConfig.java
+++ src/main/java/group_name/artifact_name/SpringConfig.java
@@ -1,34 +1,49 @@
 package group_name.artifact_name;
 
-import group_name.artifact_name.repository.JdbcMemberRepository;
-import group_name.artifact_name.repository.JdbcTemplateMemberRepository;
-import group_name.artifact_name.repository.MemberRepository;
-import group_name.artifact_name.repository.MemoryMemberRepository;
+import group_name.artifact_name.repository.*;
 import group_name.artifact_name.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+
+//    @PersistenceContext
+//    private final EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    //    private final DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memoryMemberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memoryMemberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+//    @Bean
+//    public MemberRepository memoryMemberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//    }
 
 }
--- src/main/java/group_name/artifact_name/domain/Member.java
+++ src/main/java/group_name/artifact_name/domain/Member.java
@@ -1,10 +1,17 @@
 package group_name.artifact_name.domain;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
